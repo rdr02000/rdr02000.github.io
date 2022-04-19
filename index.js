0f116ef0f47ac48e40f1064fc3c0f5d9f5020848
rdr02000 <ramel.rosales@gmail.com>
2022-04-19 12:14:25 -0400

    hnty
    fgrgr

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ setCookies();
 
 const initParams = {
   //srcDpaId: 'fa6f0599318a4912910c76d269aca923',
-  //srcDpaId: '2360e9a2-17a7-4766-b08a-a3aef372c643',
-  srcDpaId: '6441fbba-9602-4522-8ac6-bf12d1edc91a',
+  srcDpaId: '2360e9a2-17a7-4766-b08a-a3aef372c643',
+  //srcDpaId: '6441fbba-9602-4522-8ac6-bf12d1edc91a',
   cardBrands: ['mastercard', 'visa', 'amex', 'discover'],
   //cardBrands: ['discover'],
   dpaTransactionOptions: {
